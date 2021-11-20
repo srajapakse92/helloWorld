fa907d3a0eb9d37d7335e667943c4916f442d96e
sanjeeva rajapakse <sanjeeva.rajapakse92@gmail.com>
2021-11-20 16:52:24 +0530

    initial add of all files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    String mLastname;
+
+    public String getLastname() {
+        return mLastname;
+    }
+
+    public void setLastname(String lastname) {
+        mLastname = lastname;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("what");
+    }
+}
+
