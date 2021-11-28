0116bcca0ad579b8648f95b4c443da55a4f456b5
sanjeeva rajapakse <sanjeeva.rajapakse92@gmail.com>
2021-11-28 13:25:11 +0530

    unwanted

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("what");
+
+        System.out.println("Revert this");
     }
 }
 
